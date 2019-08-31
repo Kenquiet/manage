b7b8adb937ce84c1f5ba12f7e65f1026ab662e37
kenquiet <1165591789@qq.com>
2019-08-31 22:04:16 +0800

    add leftMenu_component

--- vue-app/src/views/index.vue
+++ vue-app/src/views/index.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="index">
     <headerTop/>
-    <router-view/>
+    <leftMenu/>
+    <div class="rightContainer">
+      <router-view/>
+    </div>
   </div>
 </template>
 
 <script>
   import headerTop from '../components/headerTop'
+  import leftMenu from '../components/leftMenu'
   export default {
     name: "index",
     components : {
-      headerTop
+      headerTop,
+      leftMenu
     }
   }
 </script>
@@ -20,5 +25,10 @@
     height: 100%;
     width: 100%;
     overflow: hidden;
+  }
+  .rightContainer{
+    padding-left: 180px;
+    width: 100%;
+    height: 100%;
   }
 </style>
